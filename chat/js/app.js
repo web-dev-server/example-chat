994f073dbe4f0164b1d822a1c33ef30ee12abda6
Tom Flídr <tomflidr@gmail.com>
2019-01-31 18:13:43 +0100

    websocket location protocol fix

--- chat/js/app.js
+++ chat/js/app.js
@@ -18,8 +18,9 @@ app.config(['$stateProvider', function ($router) {
 
 app.run(['$rootScope', function($rootScope) {
 	var currentFullPath = '//' + location.host + location.pathname;
+	var socketProtocol = location.protocol === 'https:' ? 'wss:' : 'ws:';
 	$rootScope.LOGIN_URL = location.protocol + currentFullPath + 'data/';
-	$rootScope.WEBSOCKET_URL = 'ws:' + currentFullPath + 'data/';
+	$rootScope.WEBSOCKET_URL = socketProtocol + currentFullPath + 'data/';
 	$rootScope.LS_USERNAME_KEY = 'username';
 	$rootScope.username = localStorage[$rootScope.LS_USERNAME_KEY] || '' ;
 }]);
